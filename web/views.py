1ec14a4d1aedb03a1aaa664c7b8b476ebae44d91
sergiunegrusa <sergiunegrusa@yahoo.com>
2022-07-13 11:30:59 +0300

    setup launch & initial page

--- web/views.py
+++ web/views.py
@@ -2,5 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
+def calculate():
+    x = 1
+    y = 2
+    return x+y
+
 def say_hello(request):
+    x = calculate()
+
     return render(request, 'hello.html', {'name': 'Sergiu'})
