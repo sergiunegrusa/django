fd5d8306a7b647d40457686d5db3ca8dc31e4e71
sergiunegrusa <sergiunegrusa@yahoo.com>
2022-07-13 17:33:37 +0300

    Create product model & add initial admin interface

--- web/models.py
+++ web/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    title = models.CharField(max_length=120) #max_length=required
+    description = models.TextField(blank=True, null=True)
+    price = models.DecimalField(max_digits=4, decimal_places=2)
+    summary = models.TextField(default="this is cool")
